b4979bd6932f72aca5ca12760ab5e2c7b632eb7d
frkn9 <80001290+frkn9@users.noreply.github.com>
2023-12-03 18:04:15 +0300

    Update and rename app.py to server.py
    Changed app.py to server.py that connects to and queries a table from the database and sends it to /api/data endpoint

---
+++ pymysql_use/server.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+from flask_cors import CORS  # Cross-Origin Resource Sharing
+import mysql.connector
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})    
+    
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Connect to MySQL database
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='05542575141',
+        database='banking'
+    )
+
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM completedacct"
+    cursor.execute(query)
+
+    #fetching all rows
+    data = cursor.fetchall()
+
+    #converting data to a list of dictionaries
+    columns = [desc[0] for desc in cursor.description]
+    result = [dict(zip(columns, row)) for row in data]
+
+    # closing the cursor and connection
+    cursor.close()
+    connection.close()
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
